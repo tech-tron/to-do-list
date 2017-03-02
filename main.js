024771e72283a22dd62e77bea6271a37d979218c
tech-tron <phillables@gmail.com>
2017-03-01 22:15:47 -0500

    google font family

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 //add quote
 $(document).ready(function(){
   var userInput = document.getElementById('userInput');
-  var listItem = document.createElement('li')
 
   getQuote();
-  count = 0;
+  var idHolder = 0;
   userInput.focus()
 
   userInput.onkeyup = function(event){
@@ -13,18 +12,20 @@ $(document).ready(function(){
     }
   }
 
+
+
+  function addListItem(text){
+    idHolder++;
+    $('#list').append('<p id="'+idHolder+'">'+text+'</p>')
+    userInput.focus();
+    userInput.select();
+  }
+
+  function getQuote(){
+    //gonna get quote from un made json
+    var jQuote = "When you want it more than you want to breath, you can do anything";
+    //hover over: show author details
+    var jAuthor
+    $('#quote').text(jQuote);
+  }
 })
-
-function addListItem(text){
-  $('#list').append('<li id="'+count+'">'+text+'</li>')
-  userInput.focus();
-  userInput.select();
-}
-
-function getQuote(){
-  //gonna get quote from un made json
-  var jQuote = "When you want it more than you want to breath, you can do anything";
-  //hover over: show author details
-  var jAuthor
-  $('#quote').text(jQuote);
-}
