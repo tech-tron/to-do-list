a2e7a9e38f4a2de2aa41761a57a9dc0c035f8b5b
tech-tron <phillables@gmail.com>
2017-03-06 00:02:52 -0500

    element.style into styleElements() function

--- main.js
+++ main.js
@@ -23,14 +23,8 @@ $(document).ready(function(){
   setInterval(updateColor, 400);
 
   function updateColor() {
-    document.getElementById('quote').style.color = color[counter];
-    document.getElementById('header').style.color = color[counter];
-    document.getElementById('header').style.border = "1px "+color[counter]+" dashed";
-    document.getElementById('title').style.border = "1px "+color[counter]+" solid";
-    document.getElementById('list').style.borderRight = "1px "+color[counter]+" dashed";
-    document.getElementById('list').style.borderLeft = "1px "+color[counter]+" dashed";
-    document.getElementById('list').style.backgroundColor = color[counter];
-    document.getElementById('battery').className = "fa fa-battery-"+counter+" fa-5x"
+    styleElements(counter);
+    nthRowUpdate(counter);
     counter++;
     if(counter < 2){
       document.getElementById('header').style.backgroundColor = "#fff";
@@ -41,12 +35,26 @@ $(document).ready(function(){
       counter = 0;
     };
   };
+  function nthRowUpdate(counter){
+
+  };
+
+  function styleElements(counter){
+    document.getElementById('quote').style.color = color[counter];
+    document.getElementById('header').style.color = color[counter];
+    document.getElementById('header').style.border = "1px "+color[counter]+" dashed";
+    document.getElementById('title').style.border = "1px "+color[counter]+" solid";
+    document.getElementById('list').style.borderRight = "1px "+color[counter]+" dashed";
+    document.getElementById('list').style.borderLeft = "1px "+color[counter]+" dashed";
+    document.getElementById('list').style.backgroundColor = color[counter];
+    document.getElementById('battery').className = "fa fa-battery-"+counter+" fa-5x"
+  };
 
   function addListItem(text){
     $('#list').append('<p id="listItem">'+text+'</p>')
     userInput.focus();
     userInput.select();
-  }
+  };
 
   function getQuote(){
     //gonna get quote from un made json
